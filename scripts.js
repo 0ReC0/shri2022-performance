55a2afe1a84b6e687116d118c200fc44ed2fe878
Kitanin Sergei <kitanin.ser@mail.ru>
2022-11-06 04:05:45 +0300

    fix: try to fix qs for items in onclick,input

--- scripts.js
+++ scripts.js
@@ -11,28 +11,44 @@
     const list = Array.from(tabs).map(node => node.dataset.id);
     const select = node.querySelector('.section__select');
 
-    function selectTab(newId) {
-      const newTab = node.querySelector(`.section__tab[data-id=${newId}]`);
-      const newPanel = node.querySelector(`.section__panel[data-id=${newId}]`);
-      const oldTab = node.querySelector('.section__tab_active');
-      const oldPanel = node.querySelector('.section__panel:not(.section__panel_hidden)');
+    const tabsDict = Array.from(tabs).reduce((acc, node) => {
+      acc[node.dataset.id] = node
+      return acc
+    }, {});
+
+    const pannels = node.querySelectorAll('.section__panel[data-id]');
+    const pannelDict = Array.from(pannels).reduce((acc, node) => {
+      acc[node.dataset.id] = node
+      return acc
+    }, {});
+
+    let oldTab = node.querySelector('.section__tab_active');
+    let oldPanel = node.querySelector('.section__panel:not(.section__panel_hidden)');
+
+    const selectTab = (newId) => {
+      const newTab = tabsDict[newId];
+      const newPanel = pannelDict[newId];
 
       selected = newId;
 
       oldTab.classList.remove('section__tab_active');
       oldTab.setAttribute('aria-selected', 'false');
       oldTab.removeAttribute('tabindex');
+
       newTab.classList.add('section__tab_active');
       newTab.setAttribute('aria-selected', 'true');
       newTab.setAttribute('tabindex', '0');
       newTab.focus({
         preventScroll: true
       });
+      oldTab = newTab
 
       oldPanel.classList.add('section__panel_hidden');
       oldPanel.setAttribute('aria-hidden', 'true');
+
       newPanel.classList.remove('section__panel_hidden');
       newPanel.setAttribute('aria-hidden', 'false');
+      oldPanel = newPanel
 
       select.value = newId;
     }
@@ -82,18 +98,21 @@
   function makeMenu(node) {
     let expanded = false;
     const links = document.querySelector('.header__links');
+    const header__menu_text = node.querySelector('.header__menu-text')
 
     node.addEventListener('click', () => {
       expanded = !expanded;
       node.setAttribute('aria-expanded', expanded ? 'true' : 'false');
-      node.querySelector('.header__menu-text').textContent = expanded ? 'Закрыть меню' : 'Открыть меню';
+      header__menu_text.textContent = expanded ? 'Закрыть меню' : 'Открыть меню';
       links.classList.toggle('header__links_opened', expanded);
       links.classList.add('header__links-toggled');
     });
   }
 
   document.addEventListener('DOMContentLoaded', () => {
-    Array.from(document.querySelectorAll('.main__devices')).forEach(makeTabs);
-    Array.from(document.querySelectorAll('.header__menu')).forEach(makeMenu);
+    const header__menu = document.querySelector('.header__menu')
+    const main__devices = document.querySelector('.main__devices')
+    makeTabs(main__devices)
+    makeMenu(header__menu)
   });
 })();
